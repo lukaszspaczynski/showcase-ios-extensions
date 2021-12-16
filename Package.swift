a1c7378472988852e3fb2bb25bd8040929fbea60
Łukasz Spaczyński <lukasz.spaczynski@dazn.com>
2021-12-16 09:55:23 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ShowcaseExtensions",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_11)],
     products: [
         .library(
             name: "ShowcaseExtensions",
